c7d95b46be5b7c57f18960159e00e6886d5eaee3
maxlanger <mail@maxlanger.de>
2020-05-19 16:20:53 +0200

    added sources

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Logo from './logo.png';
+
+const App = () => (
+  <div>
+    <h1>react-webpack-babel-boilerplate dsa</h1>
+    <img src={Logo} />
+  </div>
+);
+
+export default App;
